a17e3ce15e60941cd8cfad1955ac9f387e09f531
jakub.durys <duryskuba@wp.pl>
2022-11-02 20:34:39 +0100

    init of create user component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -5,13 +5,15 @@ import {UsersRoutingModule} from "./users-routing.module";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { UsersListComponent } from './users-list/users-list.component';
 import {SharedModule} from "../shared/shared.module";
+import { CreateUserComponent } from './create-user/create-user.component';
 
 
 
 @NgModule({
   declarations: [
     UsersComponent,
-    UsersListComponent
+    UsersListComponent,
+    CreateUserComponent
   ],
   imports: [
     CommonModule,
