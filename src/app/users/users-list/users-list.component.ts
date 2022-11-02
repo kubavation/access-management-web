65f2ca7599888c198ca777d5c3800ac905218196
jakub.durys <duryskuba@wp.pl>
2022-11-02 17:39:58 +0100

    user table

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,5 +1,8 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, ViewChild} from '@angular/core';
 import {User} from "../model/user";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-users-list',
@@ -9,7 +12,19 @@ import {User} from "../model/user";
 })
 export class UsersListComponent {
 
-  @Input() users: User[];
+  @ViewChild(MatSort) private _sort: MatSort;
+  @ViewChild(MatPaginator) private _paginator: MatPaginator;
+
+  @Input() set users(_users: User[]) {
+    this.dataSource = new MatTableDataSource<User>(_users);
+    this.dataSource.sort =  this._sort;
+    this.dataSource.paginator =this._paginator;
+  }
+
+  dataSource: MatTableDataSource<User>;
+
+  public readonly DATA_TABLE_COLUMNS = ['id', 'username'];
+
 
   constructor() { }
 
