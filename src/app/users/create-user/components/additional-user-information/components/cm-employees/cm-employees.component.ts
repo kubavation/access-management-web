de9a566c4535f3491970ddcf73ec9bbf01d917cd
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-05 21:09:26 +0100

    matDialog in employees component

--- src/app/users/create-user/components/additional-user-information/components/cm-employees/cm-employees.component.ts
+++ src/app/users/create-user/components/additional-user-information/components/cm-employees/cm-employees.component.ts
@@ -1,16 +1,13 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-cm-employees',
   templateUrl: './cm-employees.component.html',
-  styleUrls: ['./cm-employees.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./cm-employees.component.scss']
 })
 export class CmEmployeesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(public dialogRef: MatDialogRef<CmEmployeesComponent>) { }
 
 }
