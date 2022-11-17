6a1c98f99bca18cd687e791afcdd83c6d7362eb0
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-17 20:10:43 +0100

    passing form as input in modal

--- src/app/users/create-user/components/add-user-roles/modal/add-user-roles-modal/add-user-roles-modal.component.ts
+++ src/app/users/create-user/components/add-user-roles/modal/add-user-roles-modal/add-user-roles-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {RoleService} from "../../../../../../roles/service/role.service";
 import {UsersService} from "../../../../../service/users.service";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-add-user-roles-modal',
@@ -14,8 +15,13 @@ export class AddUserRolesModalComponent {
 
   userRoles$ = this.userService.getUserRoles("123");
 
+  form = this.fb.group({
+    roles: []
+  })
+
   constructor(private dialogRef: MatDialogRef<AddUserRolesModalComponent>,
               private roleService: RoleService,
+              private fb: FormBuilder,
               private userService: UsersService) { }
 
   public open(): void {
